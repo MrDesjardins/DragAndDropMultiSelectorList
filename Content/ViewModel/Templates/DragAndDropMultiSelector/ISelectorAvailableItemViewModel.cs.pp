b24dc70e4563aa6284b50350c46975254fcc4ed4
MrDesjardins <mrdesjardins@gmail.com>
2013-12-07 22:23:40 -0500

    First commit for the Nuget package

---
+++ Content/ViewModel/Templates/DragAndDropMultiSelector/ISelectorAvailableItemViewModel.cs.pp
@@ -0,0 +1,9 @@
+﻿namespace $rootnamespace$.ViewModel.Templates.DragAndDropMultiSelector
+{
+    public interface ISelectorAvailableItemViewModel<TUniqueIdentifierType, TExtendedValueType>
+    {
+      TUniqueIdentifierType ID { get; set; }
+      string Description { get; set; }
+      TExtendedValueType ExtendedProperty { get; set; }
+    }
+}
---
+++ Content/ViewModel/Templates/DragAndDropMultiSelector/MultiSelectorWithSingleValueExtendedItem.cs.pp
@@ -0,0 +1,15 @@
+namespace $rootnamespace$.ViewModel.Templates.DragAndDropMultiSelector
+{
+    public class MultiSelectorWithSingleValueExtendedItem<TUniqueIdentifierType, TExtendedValueType> : ISelectorAvailableItemViewModel<TUniqueIdentifierType, TExtendedValueType>
+    {
+
+        #region Implementation of ISelectorAvailableItemViewModel<T>
+
+        public TUniqueIdentifierType ID { get; set; }
+        public string Description { get; set; }
+        public TExtendedValueType ExtendedProperty { get; set; }
+
+        #endregion
+
+    }
+}
